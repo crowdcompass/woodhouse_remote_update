09c86cbd092a8cd66766628d9cc69ece7611eb0c
Ben Cullen-Kerney <ben@beancuke.com>
2014-01-29 02:04:47 -0800

    Listener workers reside in consuming application for now

--- lib/woodhouse_remote_update.rb
+++ lib/woodhouse_remote_update.rb
@@ -1,7 +1,5 @@
 require "woodhouse_remote_update/version"
 require "woodhouse_remote_update/sender/updater"
-require "woodhouse_remote_update/sender/response_listener"
-require "woodhouse_remote_update/receiver/remote_update_listener"
 require "woodhouse_remote_update/receiver/remote_update"
 
 module WoodhouseRemoteUpdate
